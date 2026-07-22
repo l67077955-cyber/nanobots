ed8cfba7c884df516e7f89087aeb370c51662a61
nanobot-dev <dev@nanobot.local>
2026-07-22 10:13:23 +0800

    chore: untrack memory_palace/ runtime data + remove one-off scripts
    - git rm -r --cached memory_palace/ (chromadb runtime data shouldn't be in git)
    - add memory_palace/ to .gitignore
    - rm a.sh fix_single_agent.py test_interrupt_sender.py test_mp*.py
      (all zero-reference one-off debug scripts)

--- test_mp3.py
+++
@@ -1,5 +0,0 @@
-import chromadb
-c1 = chromadb.PersistentClient(path='/root/.nanobot/mempalace/palace')
-print("c1 created")
-c2 = chromadb.PersistentClient(path='/root/.nanobot/mempalace/palace')
-print("c2 created")
